2bd1c9b7ca3c7c1087584693c6499183ffe0c4e1
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-03 14:18:03 +0530

    Add user object example with comments for better understanding

--- 02_learning.js
+++ 02_learning.js
@@ -20,3 +20,17 @@ let a = 5;
 change(a);
 
 console.log(a)
+
+//-------------------------------------------
+
+const user = {
+  name: "Amit",
+  city: "Delhi",
+  isActive: true // good approach makes debugging faster and 
+                 // other dev to uderstand how object is created fater.
+};
+
+// adding property later
+// user.isActive = true; // bad approach while making a new object
+
+
