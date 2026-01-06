7096f53a5e36adb626eac275b9feffa887072eaf
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-06 20:14:23 +0530

    Add string methods demonstration in 04_learning.js

---
+++ 04_learning.js
@@ -0,0 +1,11 @@
+// STRING METHODS
+
+let str = "Muktesh is top 1% software engineer"
+
+console.log(str.length); //length of string
+
+console.log(str[0]);
+
+console.log(str.toUpperCase());
+
+console.log(str.toLowerCase());
