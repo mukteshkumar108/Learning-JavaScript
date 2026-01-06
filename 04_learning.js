6d6329c9724340f73c45f689270f76d95e78651e
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-06 20:18:29 +0530

    Add additional string method examples in 04_learning.js

--- 04_learning.js
+++ 04_learning.js
@@ -9,3 +9,18 @@ console.log(str[0]);
 console.log(str.toUpperCase());
 
 console.log(str.toLowerCase());
+
+console.log(str.includes("top"));
+// true
+
+console.log(str.indexOf("software"));
+// 18
+
+console.log(str.startsWith("Muktesh"));
+// true
+
+console.log(str.endsWith("engineer"));
+// true
+
+console.log(str.slice(8, 10));
+// "is"
