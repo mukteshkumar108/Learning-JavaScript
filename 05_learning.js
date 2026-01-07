228796cc9ee7ee4c94e8bc141c9828edb9fc9e4c
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-07 22:50:14 +0530

    Add initial examples for truthy and falsy values in 05_learning.js

---
+++ 05_learning.js
@@ -0,0 +1,7 @@
+// Day 7 – Truthy & Falsy in JavaScript
+
+console.log(Boolean(0));       // false
+console.log(Boolean(""));      // false
+console.log(Boolean("hello")); // true
+console.log(Boolean([]));      // true
+console.log(Boolean({}));      // true
