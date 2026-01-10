0e0ba06477b3446af3117625f912a95d3721a1c7
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-11 00:36:13 +0530

    Add falsy values explanations in 08_revision.js

---
+++ 08_revision.js
@@ -0,0 +1,6 @@
+// numbers mein sirf 0 falsy hota hai
+// Nan is also falsy
+// '' empty string is falsy
+// ' ' this empty string is true because empty space is also a character
+// undefined is falsy
+// null is also falsy
