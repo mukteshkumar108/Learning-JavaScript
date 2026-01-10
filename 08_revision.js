39796d2657b70e8f835a24559a9e62f3e9289b61
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-11 00:58:26 +0530

    Add explanations and examples for comparison operators in 08_revision.js

--- 08_revision.js
+++ 08_revision.js
@@ -4,3 +4,36 @@
 // ' ' this empty string is true because empty space is also a character
 // undefined is falsy
 // null is also falsy
+
+// Comparison Operators
+
+// == equal to (equality)
+
+let user1 = 7
+let user2 = '7'
+let res = user1 == user2 // this == is used less in modern js code because it do type conversion which converts string into number(implicit conversion)
+let res2 = user1 === user2 // this === is used more in modern js code
+
+console.log(res); // true
+console.log(res2); // false //explicit conversion allowed hai
+
+//!= not equal opposit of == equal to
+
+let res3 = user1 != user2
+let res4 = user1 !== user2
+
+console.log(res3); // false
+console.log(res4); //true
+
+let chhote = 1
+let badhe = 1
+
+let compare = chhote >= badhe
+let compare2 = chhote <= badhe
+let compare3 = chhote > badhe
+let compare4 = chhote < badhe
+
+console.log(compare);
+console.log(compare2);
+console.log(compare3);
+console.log(compare4);
