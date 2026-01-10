5467fc14439cba1a21683dd9f5f4c398e8852ab5
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-10 22:33:14 +0530

    Add Math object explanations and examples in 07_revision.js

---
+++ 07_revision.js
@@ -0,0 +1,32 @@
+//Math object is a built in object in JavaScript
+
+// Remainder Operator: %
+// 13 % 2 = 1
+
+// Division Operator: /
+// 15 / 4 = 3.75
+
+// Exponentiation Operator: ** //this is a part of ES7
+// 3 ** 4 = 81
+
+console.log(Math.sqrt(25));// 5 this function shows square root of a number
+
+console.log(Math.pow(4,8));// 65536 same as Exponential Operator
+
+console.log(Math.floor(12.5)); //12 removes decimal
+
+console.log(Math.ceil(2.5)); //3 ceil meaning upar uthana
+console.log(Math.ceil(-2.5)); //-2 
+
+console.log(Math.round(2.4)); // ghata dega
+console.log(Math.round(2.5)); // badha dega
+
+console.log(Math.random()); // returns numbers btw 0 and 1
+                            // 0 aa sakta hai but 1 kabhi nhi aayega
+
+console.log(Math.random()*21);
+
+console.log(Math.floor()*21); // NaN
+
+console.log(typeof Infinity); // number
+
