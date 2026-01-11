efd4c102e2a400c3fb743f68531dda0c65f13539
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-11 17:34:05 +0530

    Add user age and username logging with conditional messages in 10_revision.js

---
+++ 10_revision.js
@@ -0,0 +1,15 @@
+const username = 'Muktesh'
+const userAge = 25
+
+console.log(`Name: ${username}`);
+console.log(`Age: ${userAge}`);
+
+
+if (userAge >= 18 && username <= 24) {
+    console.log('He is a College student.');
+}
+if(userAge >= 25 && userAge <= 45){
+    console.log('He is Working professional');
+}
+
+
