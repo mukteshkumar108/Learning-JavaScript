fb55b41d65267f16ed7372df39721d294f9e9d36
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-11 18:06:54 +0530

    Refactor username and userAge initialization with default values and fix age condition check

--- 10_revision.js
+++ 10_revision.js
@@ -1,11 +1,18 @@
-const username = 'Muktesh'
-const userAge = 25
+let username = 'Bhondu'// = 'Muktesh'
+let userAge = 20// = 25
+
+// Set default values if user doesn't enter any name or age
+if (!username) {
+    username = 'Muktesh';
+}
+if (!userAge) {
+    userAge = 20;
+}
 
 console.log(`Name: ${username}`);
 console.log(`Age: ${userAge}`);
 
-
-if (userAge >= 18 && username <= 24) {
+if (userAge >= 18 && userAge <= 24) {
     console.log('He is a College student.');
 }
 if(userAge >= 25 && userAge <= 45){
