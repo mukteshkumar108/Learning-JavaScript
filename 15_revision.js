afe1427b070764403210b7e8b4f66ee201a87221
Muktesh kumar <mukteshkumar108@gmail.com>
2026-01-13 23:49:36 +0530

    added another code of .filter() and learned about .reduce for js in 15_revision.js

--- 15_revision.js
+++ 15_revision.js
@@ -30,3 +30,37 @@ const evenNumbers = numbers1.filter(num => num % 2 === 0);
 
 console.log(evenNumbers);
 // [2, 4, 6]
+
+
+const people = [
+  { name: "Aman", age: 17 },
+  { name: "Ravi", age: 21 },
+  { name: "Neha", age: 19 }
+];
+
+const adults = people.filter(person => person.age >= 18);
+
+console.log(adults);
+// [{ name: "Ravi", age: 21 }, { name: "Neha", age: 19 }]
+
+// Sum of numbers .reduce()
+
+const numbers2 = [10, 20, 30];
+
+const total = numbers2.reduce((sum, num) => {
+  return sum + num;
+}, 0);
+
+console.log(total);
+// 60
+
+// Count occurrences
+const fruits = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+const fruitCount = fruits.reduce((count, fruit) => {
+  count[fruit] = (count[fruit] || 0) + 1;
+  return count;
+}, {});
+
+console.log(fruitCount);
+// { apple: 3, banana: 2, orange: 1 }
