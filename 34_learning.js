7d06bded8354d918fc42b686bde4e7b729f0f8f5
Muktesh <mukteshkumar108@gmail.com>
2026-06-07 15:45:17 +0000

    destructuring in objects and arrays

--- 34_learning.js
+++ 34_learning.js
@@ -14,6 +14,28 @@ const message1 = `Here we will use ${backticks}`;
 
 console.log(message1);
 
+let userName = {
+    name : "Muktesh",
+    rollNumber : 25,
+    class : "Special grade",
+}
 
+console.log(userName.name);
 
+// const { name, age } = user;
+const user = {
+    name : "Akaash",
+    state: "Bihar",
+    country: "India",
+    age: 21,
+}
+const { name, age } = user;
+
+console.log({ name, age});
+
+const ashramGang = ['PanMasala', 'Muktesh', 'EveningHandsome', 'Rand1', 'Rand2','Playboi'];
+
+const [first, ,third] = ashramGang;
+
+console.log([first,third]);
 
