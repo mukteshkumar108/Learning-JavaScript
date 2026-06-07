663627eaed555e7a611b264ec905fe8b319688ff
Muktesh <mukteshkumar108@gmail.com>
2026-06-07 22:21:21 +0000

    .map(), .filter(), .reduce() codex examples

--- 34_learning.js
+++ 34_learning.js
@@ -39,3 +39,30 @@ const [first, ,third] = ashramGang;
 
 console.log([first,third]);
 
+// Reduce work is too combine all the array elements together until you left with single value
+const numbers = [34 , 45 , 57 , 90];
+
+const sum = numbers.reduce((total, num) => {
+    return total + num;
+}, 0);
+
+console.log(sum)
+
+// Filter .filter() is used to filter array by checking it each item and check if it passes the specific condition
+// (a test to check it item is true or false). If an item passes, it get into new array, and the items that failed gets
+// kicked out
+
+const number2 = [2,3,67,65,43,34,22];
+
+const even = number2.filter(num => num % 2 === 0);
+
+console.log(even);
+
+// .map() uses a specific funtion it got and run that function on each item of an array and gives new array with transformed values.
+
+const doubledValue = numbers.map(num => num * 5);
+
+console.log(doubledValue)
+
+//Spread operator and Rest operator might look same but are different
+
