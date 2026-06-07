a56771e19e8f003698eb5c263aba723977f1cc3a
Muktesh <mukteshkumar108@gmail.com>
2026-06-07 14:59:01 +0000

    Arrow function and template literal

---
+++ 34_learning.js
@@ -0,0 +1,19 @@
+// Arrow function => Function ko likhne ka short and moder style
+
+//Template literal => Function ke andar variable dalne ka clean way
+
+const M = "Muktesh";
+
+const message = "Hello " +  M + ", Apka din kaisa hai?"; //old way
+
+console.log(message);
+
+const backticks = "backtick ``";
+
+const message1 = `Here we will use ${backticks}`;
+
+console.log(message1);
+
+
+
+
